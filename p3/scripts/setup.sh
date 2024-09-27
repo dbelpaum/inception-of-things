2c71f00c840a358553e3e4614f1dc6d379ca9c18
Remi DARNET <redarnet@made-f0Br3s3.clusters.42paris.fr>
2024-09-27 12:05:50 +0200

    fin du script

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -3,47 +3,34 @@
 # Uninstall old versions
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get -y install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
 
 # Install docker
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Install k3d
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+# # Install k3d
+# curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-# Create a k3d cluster
-sudo k3d cluster create mycluster
+# # Create a k3d cluster
+# sudo k3d cluster create mycluster
 
-# Install kubectl
-sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# # Install kubectl
+# sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-# Install Argo CD
-sudo kubectl create namespace argocd
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# # Install Argo CD
+# sudo kubectl create namespace argocd
+# sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# Wait for Argo CD to be fully deployed
-echo "Waiting for Argo CD to be fully deployed..."
-sudo kubectl wait --for=condition=available --timeout=600s deployment/argocd-server -n argocd
+# # Wait for Argo CD to be fully deployed
+# echo "Waiting for Argo CD to be fully deployed..."
+# sudo kubectl wait --for=condition=available --timeout=600s deployment/argocd-server -n argocd
 
-# Install Argo CD CLI
-sudo curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-sudo chmod +x /usr/local/bin/argocd
+# # Install Argo CD CLI
+# sudo curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+# sudo chmod +x /usr/local/bin/argocd
 
-# Argo CD UI
-sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+# # Argo CD UI
+# sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 &
 
-# Print Argo CD initial admin password
-sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+# # Print Argo CD initial admin password
+# sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
--- p3/scripts/setups-argo.sh
+++ p3/scripts/setups-argo.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get -y install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 # # Install docker
  sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
@@ -34,6 +47,8 @@ sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 &
 
 sudo kubectl create namespace dev
 
-sudo kubectl apply -f argo-app.yaml
+sudo kubectl apply -f ../config/argo-app.yaml
+
+sleep 20
 
 sudo kubectl port-forward svc/playground-service 8888:80 -n dev
